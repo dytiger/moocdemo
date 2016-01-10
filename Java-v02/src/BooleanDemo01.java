b2b944f3930891a0a5c6a8acc7c04b6813e6f2b2
dytiger <spring6225>
2016-01-10 16:54:45 +0800

    添加内容

---
+++ Java-v02/src/BooleanDemo01.java
@@ -0,0 +1,11 @@
+public class BooleanDemo01 {
+    public static void main(String[] args) {
+        // 布尔类型字面量只能取true或false两个值
+        boolean b1 = true;
+        b1 = false;
+
+        // 与C语言不同，true和false不能与数字相互转换
+        // b1 = 1;// 编译错误
+        // b1 = 0;// 编译错误
+    }
+}
