a639525f146f18dd4ef919c48711f30a1bc56318
dytiger <spring6225>
2016-01-10 17:18:00 +0800

    添加内容

---
+++ Java-v02/src/AreaOfCircle.java
@@ -0,0 +1,10 @@
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        double r=5;
+        double pi = 3.1415926;
+        double area;
+
+        area = pi * r * r;
+        System.out.println("半径为" + r + "的圆面积是" + area);
+    }
+}
