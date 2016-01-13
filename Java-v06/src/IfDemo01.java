edfe2cc4e6c709871f9953fc9d4f1922c523779b
dytiger <spring6225>
2016-01-13 12:45:08 +0800

    添加内容

---
+++ Java-v06/src/IfDemo01.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+/**
+ * if语句
+ */
+public class IfDemo01 {
+    public static void main(String[] args) {
+        // 得到生成随机值的对象
+        Random random = new Random();
+        // 随机生成boolean类型的值并赋给sunny变量
+        boolean sunny = random.nextBoolean();
+        // 对if括号中的逻辑值进行判断，如果为true则执行if语句块中的语句
+        // 否则执行if语句块后的其它语句
+        // 本例中的if(sunny==true)或以简写为if(sunny)
+        // 如果if语句块中只有一条语句，则可以省略定义if块的一对{}，
+        // 但出于程序的可读性考虑，不建议这样做
+        if(sunny==true){
+            System.out.println("今天是晴天，可以去爬山。");
+        }
+        System.out.println("回家睡觉");
+    }
+}
